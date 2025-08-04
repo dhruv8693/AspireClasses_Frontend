770aa996848e33dd1e3491ae3ec1617d58f994f5
Dhruv <mittaldhruv41@gmail.com>
2025-08-04 09:26:37 +0530

    Structural_Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
-import Legacy from "./components/Legacy/Legacy";
-import Creators from "./components/Creators/Creators";
-import Features from "./components/Features/Features";
-import Pricing from "./components/Pricing/Pricing";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
+import Navbar from "./Pages/Landing_Page/components/Navbar/Navbar";
+import Hero from "./Pages/Landing_Page/components/Hero/Hero";
+import Legacy from "./Pages/Landing_Page/components/Legacy/Legacy";
+import Creators from "./Pages/Landing_Page/components/Creators/Creators";
+import Features from "./Pages/Landing_Page/components/Features/Features";
+import Pricing from "./Pages/Landing_Page/components/Pricing/Pricing";
+import Contact from "./Pages/Landing_Page/components/Contact/Contact";
+import Footer from "./Pages/Landing_Page/components/Footer/Footer";
 import "./App.css";
 
 function App() {
